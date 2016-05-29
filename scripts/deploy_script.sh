164d7d5a09dc8f4750256148cc6be472dec65ada
john <john@patterninc.co>
2016-05-29 10:00:17 -0400

    track changes test

--- scripts/deploy_script.sh
+++ scripts/deploy_script.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+ssh ubuntu@ec2-54-201-96-53.us-west-2.compute.amazonaws.com
+sudo cd /var/www/
+sudo git pull https://github.com/JohnnyDangerously/flasky/
+sudo restart uwsgi
+sudo restart nginx
