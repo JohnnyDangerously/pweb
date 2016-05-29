3d14fa72f127b7824ce64238bb55aaae24191f27
john <john@patterninc.co>
2016-05-28 20:09:21 -0400

    track changes test

--- flasky.py
+++ flasky.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def flasky():
-    return "go home max"
+    return "go home max this is another test"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
