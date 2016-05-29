b5ab52e1dd831d942e09b1274a6c8dff6f0ca352
john <john@patterninc.co>
2016-05-29 11:12:07 -0400

    track changes test

--- scripts/deploy_script.sh
+++ scripts/deploy_script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 ssh ubuntu@ec2-54-201-96-53.us-west-2.compute.amazonaws.com <<EOF
   cd /var/www/
-  sudo git pull https://github.com/JohnnyDangerously/flasky.git
+  sudo git pull
   sudo restart uwsgi
   sudo restart nginx
   exit
