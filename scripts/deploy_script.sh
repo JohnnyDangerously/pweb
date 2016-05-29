2c91ea9c3c73b4a4c43c1f1eb9559658b1937057
john <john@patterninc.co>
2016-05-29 12:52:18 -0400

    changed application to pweb

--- scripts/deploy_script.sh
+++ scripts/deploy_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 ssh ubuntu@ec2-54-201-96-53.us-west-2.compute.amazonaws.com <<EOF
-  cd /var/www/
+  cd /var/www/pweb/
   sudo git pull
   sudo restart uwsgi
   sudo restart nginx
